ef2d644ac3bdf29cd73dfe8608d157954bc3db2e
金井則通(Norimichi Kanai) <k.norimichi9999@i.softbank.jp>
2016-04-30 15:42:12 +0900

    Create c043006.py

---
+++ c043006.py
@@ -0,0 +1,15 @@
+# 1 / 9801 = ?
+i = 0;
+A = 0;
+a = 1;
+b = 9801;
+while(i < 203):
+    A = a / b;
+    if(A != 0):
+        a = a - A * b;
+    a = a * 10;
+    print A;
+    i = i + 1;
+    # i++ -> SyntaxError: invalid syntax
+# -> comment -> '//' is SyntaxError: invalid syntax
+# python c043006.py > c043006.txt
